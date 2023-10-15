e57a1a1671756c7225fbde05e3feccb55cba712b
jorodrig96 <jo.rodrig96@gmail.com>
2023-10-14 18:24:21 -0700

    created some API tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,17 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import App from './App'
+
+beforeEach(() => {
+    // sets everything back to initial state before each test
+    fetch.resetMocks()
+  })
 
 
-
+describe('tests for github api', ()=> {
+    test("receives GitHub name from GitHub REST API using jest fetch mock", async () => {
+        fetch.mockResponseOnce(JSON.stringify({name: 'jorodrig96'}))
+        render(<App />)
+        const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+        expect(gitHubName).toHaveTextContent('jorodrig96')
+      })})
+    
